9a1f2b6668a873ea0203fa46254eeb3e515e12e6
Harry <hjwp2@cantab.net>
2020-05-19 00:34:21 +0100

    two players ok

--- test_ttt.py
+++ test_ttt.py
@@ -1,3 +1,4 @@
+import pytest
 from textwrap import dedent
 import ttt
 
@@ -56,3 +57,22 @@ def test_second_turn_is_O():
         .X.
         ..O
         """).strip()
+
+def test_third_turn():
+    board0 = ttt.Board()
+    board1 = board0.take_turn(1,1)
+    board2 = board1.take_turn(2,2)
+    board3 = board2.take_turn(0,2)
+    assert board3.draw() == dedent("""
+        ...
+        .X.
+        X.O
+        """).strip()
+
+
+@pytest.mark.xfail()
+def test_cannot_play_on_already_played_position():
+    board0 = ttt.Board()
+    board1 = board0.take_turn(1,1)
+    with pytest.raises(Exception):
+        board1.take_turn(1,1)
--- ttt.py
+++ ttt.py
@@ -1,18 +1,24 @@
+
 class Board:
-    def __init__(self, state=None):
-        self.state = state
+    def __init__(self, Xs=None, Os=None):
+        self.Xs = Xs or []
+        self.Os = Os or []
 
     def draw(self):
         result = ''
         for y in range(3):
             for x in range(3):
-                if (x, y) == self.state:
+                if (x, y) in self.Xs:
                     result += 'X'
+                elif (x, y) in self.Os:
+                    result += 'O'
                 else:
                     result += '.'
             result += '\n'
         return result.strip()
 
     def take_turn(self, x, y):
-        return Board((x, y))
+        if self.Xs <= self.Os:
+            return Board(self.Xs + [(x,y)], self.Os)
+        return Board(self.Xs, self.Os + [(x, y)])
 
