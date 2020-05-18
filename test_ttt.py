1fb2c60cd99a30c94d6bab84394269c615c2a951
Harry <hjwp2@cantab.net>
2020-05-19 00:25:11 +0100

    got first turn

--- test_ttt.py
+++ test_ttt.py
@@ -9,3 +9,50 @@ def test_starting_board():
         ...
         """).strip()
 
+def test_first_turn_is_X():
+    board = ttt.Board()
+    board = board.take_turn(0,0)
+    assert board.draw() == dedent("""
+        X..
+        ...
+        ...
+        """).strip()
+
+def test_take_turn_handles_x_and_y():
+    board = ttt.Board()
+    board = board.take_turn(1,1)
+    assert board.draw() == dedent("""
+        ...
+        .X.
+        ...
+        """).strip()
+
+def test_take_turn_handles_x():
+    board = ttt.Board()
+    board = board.take_turn(2,0)
+    assert board.draw() == dedent("""
+        ..X
+        ...
+        ...
+        """).strip()
+
+
+def test_take_turn_handles_y():
+    board = ttt.Board()
+    board = board.take_turn(0,2)
+    assert board.draw() == dedent("""
+        ...
+        ...
+        X..
+        """).strip()
+
+
+def test_second_turn_is_O():
+    board0 = ttt.Board()
+    board1 = board0.take_turn(1,1)
+    board2 = board1.take_turn(2,2)
+    assert board2.draw() == dedent("""
+        ...
+        .X.
+        ..O
+        """).strip()
--- ttt.py
+++ ttt.py
@@ -1,4 +1,18 @@
 class Board:
+    def __init__(self, state=None):
+        self.state = state
+
     def draw(self):
-        return '\n'.join(['...'] * 3)
+        result = ''
+        for y in range(3):
+            for x in range(3):
+                if (x, y) == self.state:
+                    result += 'X'
+                else:
+                    result += '.'
+            result += '\n'
+        return result.strip()
+
+    def take_turn(self, x, y):
+        return Board((x, y))
 
